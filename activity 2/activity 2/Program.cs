68659a7dbe993cb7b77c0ea44c3056c085e0b4f1
nejabatKairi <91402881+nejabatKairi@users.noreply.github.com>
2022-10-14 00:20:00 +0800

    Add files via upload

---
+++ activity 2/activity 2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace activity_2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Users user = new Users();
+			user.GetUserDetails();
+			Console.WriteLine("Press Enter Key to Exit..");
+		
+		Console.ReadKey();
+		}
+}
+}
+public class Users
+{
+	public string name = "Julia";
+public int age = 25;
+	public void GetUserDetails()
+	{
+		Console.WriteLine("Name: {0}, Age: {1}", name, age);
+	}
+}
