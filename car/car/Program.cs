a40d04830d981daabf1f613c92056e2107cb1931
nejabatKairi <91402881+nejabatKairi@users.noreply.github.com>
2022-10-14 00:03:25 +0800

    Add files via upload
    Activity 1

---
+++ car/car/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace car
+{
+	//Kairi Tabajen
+	// The class
+	public class Car
+	{
+		// Class members
+		public string model; // field
+		public string color; // field
+		public int maxSpeed; // field
+		public int year; // field
+		public void fullThrottle() // method
+		{
+			Console.WriteLine("**************************");
+			Console.WriteLine(model + " is going as fast as it can!");
+			Console.WriteLine(color + " is the color of  " + model);
+			Console.WriteLine(maxSpeed + " is the max speed of  " + model);
+			Console.WriteLine(year + " is the model year of  " + model);
+			Console.WriteLine("**************************");
+			Console.WriteLine("sorry sir late ko na pass");
+		}
+	}
+	public class Cars
+	{
+		public static void Main(string[] args)
+		{
+			Car ford = new Car();
+			ford.model = "Mustang";
+			ford.color = "red";
+			ford.year = 1969;
+			ford.maxSpeed = 250;
+			Car honda = new Car();
+			honda.model = "Brio";
+			honda.color = "orange";
+			honda.year = 2019;
+			honda.maxSpeed = 300;
+			Car ferrari = new Car();
+			ferrari.model = "Ferrari 812 GTS";
+			ferrari.color = "black";
+			ferrari.year = 2020;
+			ferrari.maxSpeed = 400;
+			Console.WriteLine(ford.model);
+			Console.WriteLine(honda.model);
+			Console.WriteLine(ferrari.model);
+			honda.fullThrottle();
+			Console.WriteLine("*************");
+			ford.fullThrottle();
+			Console.WriteLine("*************");
+			ferrari.fullThrottle();
+
+			Console.ReadKey();
+		}
+	}
+}
