59458dd2bc89321d077d4d47e7c39ed606335e77
nejabatKairi <91402881+nejabatKairi@users.noreply.github.com>
2022-10-14 18:58:10 +0800

    Add files via upload

---
+++ module4/module4/Person.cs
@@ -0,0 +1,16 @@
+﻿namespace module4
+{
+	public class Person
+	{
+		public string fname;
+		public string lname;
+
+		public Person(string fname, string lname)
+		{
+			this.fname = fname;
+			this.lname = lname;
+		}
+
+		public int yearsOld { get; internal set; }
+	}
+}
---
+++ module4/module4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+//Prgrammer Kairi Tabajen
+//Au-2BSIT-A
+public class Person
+{
+    // Class members
+    public string fname; // field
+    public string lname; // field
+    public int birthYear; // field
+    public int Age; // field
+    public void fullName() // method
+    {
+        //Console.WriteLine("**************************");
+        Console.WriteLine("Hello " + fname + " " + lname);
+
+        Console.WriteLine("Your age are " + " " + Age + " " + " years old.");
+
+        Console.WriteLine("*********************");
+
+        Console.WriteLine("Your first name is" + " " + fname);
+        Console.WriteLine("Your last name is" + " " + lname);
+        Console.WriteLine("Your birth year is " + " " + birthYear);
+
+    }
+
+    public class Name
+    {
+        public static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            person1.fname = "Angel";
+            person1.lname = "Locsin";
+            person1.Age = 30;
+            person1.birthYear = 1992;
+            person1.fullName();
+
+            Console.ReadKey();
+
+        }
+    }
+}
+
